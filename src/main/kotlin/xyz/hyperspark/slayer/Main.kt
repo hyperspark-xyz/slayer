47a0ed3d98116a5bef731eb9fe06a083a957b340
Aaron Marshall <hi@imaaronnicetomeetyou.me>
2022-07-09 16:45:30 +1000

    Initial commit

---
+++ src/main/kotlin/xyz/hyperspark/slayer/Main.kt
@@ -0,0 +1,17 @@
+package xyz.hyperspark.slayer
+
+import org.bukkit.Bukkit
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main: JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+        super.onEnable();
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+        super.onDisable();
+    }
+}
